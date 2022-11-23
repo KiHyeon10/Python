7cf925b08f16ff6627f9c7f54314f577966aa489
Kwon_Ki_Hyeon <96440843+KiHyeon10@users.noreply.github.com>
2022-11-23 14:46:05 +0900

    Delete JAVA directory

--- JAVA/Hello.java
+++
@@ -1,8 +0,0 @@
-class Apple { }
-class Fox { }
-public class Hello{
-    public static void main(String[] args) {
-        Apple a = new Apple();
-        //System.out.println(a instanceof Fox);
-    }
-}
