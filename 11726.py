b66cab80a80f1361ec4b9be4d3d731b9cfbf2fcb
Kwon_Ki_Hyeon <96440843+KiHyeon10@users.noreply.github.com>
2024-08-21 18:20:59 +0900

    Baek_Joon

---
+++ 11726.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+def Find(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    
+    num = [0] * (n + 1)  # n+1 크기의 리스트 생성
+    num[1] = 1
+    num[2] = 2
+    
+    for i in range(3, n + 1):
+        num[i] = (num[i-1] + num[i-2]) % 10007  # 모듈로 연산을 반복문 내부에서 적용
+    
+    return num[n]
+
+print(Find(N))
