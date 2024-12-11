33568e6186f583eac20a758b70aa4b09219e0bcf
Kwon_Ki_Hyeon <96440843+KiHyeon10@users.noreply.github.com>
2024-12-11 21:59:26 +0900

    Baek_Joon

--- 1002.py
+++ 1002.py
@@ -1,11 +1,22 @@
 import sys
+import math
+
 input = sys.stdin.readline
 
 a = int(input())
 point = []
-for j in a:
-    point[j] = map(int, input().split())
+for _ in range(a):
+    point = list(map(int, input().split()))
+    line = math.sqrt(abs((point[0] - point[3])**2) + abs((point[1] - point[4])**2))
 
-for i in len(point):
-    for j in len(point[i]):
-        if 
+    if line == 0 and point[2] == point[5]:
+        print(-1)
+    else:
+        if point[2] + point[5] > line > abs(point[2] - point[5]):
+            print(2)
+        
+        elif point[2] + point[5] == line or abs(point[2] - point[5]) == line:
+            print(1)
+        
+        else:
+            print(0)
